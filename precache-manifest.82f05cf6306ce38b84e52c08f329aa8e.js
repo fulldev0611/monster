6d0fe4eba1df766d46d886589c66c42b9b37318b
mingshi015 <ming.shi.dev@outlook.com>
2022-10-24 09:29:35 +0800

    Updates

---
+++ precache-manifest.82f05cf6306ce38b84e52c08f329aa8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216b69b909eef7531261452c3aa2797b",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "1f934261a36755838da9",
+    "url": "/monster/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "c97f49fb9758b4c690d7",
+    "url": "/monster/static/js/2.e0acc4ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster/static/js/2.e0acc4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f934261a36755838da9",
+    "url": "/monster/static/js/main.df2bbfbe.chunk.js"
+  },
+  {
+    "revision": "8124470e112c6eec1692",
+    "url": "/monster/static/js/runtime-main.df34a1e3.js"
+  }
+]);
